614ac9516b1ba2c6e12095b5e18837f418d1588e
naratakahito <gooddaytnn@gmail.com>
2013-01-20 18:33:07 +0900

    Initial Commit

---
+++ templater/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  templater
+//
+//  Created by 奈良 貴仁 on 2013/01/20.
+//  Copyright (c) 2013年 奈良 貴仁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ templater/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  templater
+//
+//  Created by 奈良 貴仁 on 2013/01/20.
+//  Copyright (c) 2013年 奈良 貴仁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
